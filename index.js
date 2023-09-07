0f521f1b87b7b53d7704f1e1173ab167ea26b057
Aditya Tiwari <pubgmlord25@gmail.com>
2023-09-07 17:36:05 +0530

    added delete on time intervals

--- index.js
+++ index.js
@@ -70,6 +70,69 @@ app.get("/data/:type/:value", async (req, res) => {
   res.json(data);
 });
 
+app.delete("/data/:type/:value", async (req, res) => {
+  let type = req.params.type;
+  let value = parseInt(req.params.value, 10);
+  let start, end;
+
+  switch (type) {
+    case "year":
+      start = new Date(value, 0, 1);
+      end = new Date(value + 1, 0, 1);
+      break;
+    case "month":
+      start = new Date(new Date().getFullYear(), value - 1, 1);
+      end = new Date(new Date().getFullYear(), value, 1);
+      break;
+    case "week":
+      start = new Date();
+      start.setDate(start.getDate() - 7 * value); // gets start of the week, assuming value is the week number of the year
+      end = new Date(start.getTime() + 7 * 24 * 3600 * 1000);
+      break;
+    case "day":
+      start = new Date(new Date().getFullYear(), new Date().getMonth(), value);
+      end = new Date(
+        new Date().getFullYear(),
+        new Date().getMonth(),
+        value + 1
+      );
+      break;
+    default:
+      return res.status(400).send("Invalid type provided");
+  }
+
+  try {
+    await TimeSeries.deleteMany({ date: { $gte: start, $lt: end } });
+    res.send(`Data for the specified period ${type} = ${value} has been deleted.`);
+  } catch (error) {
+    res.status(500).send("Error occurred while deleting data.");
+  }
+});
+
+// DELETE data based on a specific time period
+app.delete("/data", async (req, res) => {
+  const { startDate, endDate } = req.query;
+
+  if (!startDate || !endDate) {
+    return res.status(400).send("Both startDate and endDate query parameters are required.");
+  }
+
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+
+  if (isNaN(start.getTime()) || isNaN(end.getTime())) {
+    return res.status(400).send("Invalid startDate or endDate provided.");
+  }
+
+  try {
+    await TimeSeries.deleteMany({ date: { $gte: start, $lt: end } });
+    res.send(`Data from ${startDate} to ${endDate} has been deleted.`);
+  } catch (error) {
+    res.status(500).send("Error occurred while deleting data.");
+  }
+});
+
+
 mongoose
   .connect("mongodb://0.0.0.0:27017/Timeseries")
   .then(() => {
